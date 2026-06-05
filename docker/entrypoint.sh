2b5ce80d82d474c1bacefdf3f3ee523a3e71f4d6
troy.stmx <troy.stmx@gmail.com>
2026-06-05 22:05:57 +0800

    🐛 fix: ALTER TEXT SEARCH CONFIGURATION 重复执行时报 duplicate key 错误

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -141,7 +141,7 @@ setup_database() {
   log "Installing extension: zhparser (Chinese text search)"
   su - "${PG_SYS_USER}" -c "${PG_BIN}/psql -h ${PG_HOST} -p ${PG_PORT} -d ${PG_DB} -c 'CREATE EXTENSION IF NOT EXISTS zhparser'"
   su - "${PG_SYS_USER}" -c "${PG_BIN}/psql -h ${PG_HOST} -p ${PG_PORT} -d ${PG_DB} -c 'DO \$do\$ BEGIN CREATE TEXT SEARCH CONFIGURATION chinese_zh (PARSER = zhparser); EXCEPTION WHEN others THEN NULL; END \$do\$'"
-  su - "${PG_SYS_USER}" -c "${PG_BIN}/psql -h ${PG_HOST} -p ${PG_PORT} -d ${PG_DB} -c \"ALTER TEXT SEARCH CONFIGURATION chinese_zh ADD MAPPING FOR n,v,a,i,e,l WITH simple\""
+  su - "${PG_SYS_USER}" -c "${PG_BIN}/psql -h ${PG_HOST} -p ${PG_PORT} -d ${PG_DB} -c 'DO \$do\$ BEGIN ALTER TEXT SEARCH CONFIGURATION chinese_zh ADD MAPPING FOR n,v,a,i,e,l WITH simple; EXCEPTION WHEN others THEN NULL; END \$do\$'"
   ok "zhparser and Chinese text search configuration installed"
 }
 
